37d9a00747892546a986cb8337bb05ee3c2fa83f
girnarhitesh <hiteshgirnar99@gmail.com>
2025-08-11 17:17:35 +0530

    code change

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-require('dotenv').config();
+
 
 const Product = require('./models/Product');
 
